cfdf4b2a00fef9f76957c77f15154e642121a179
abdullokhonz <abdullokhon1206@gmail.com>
2025-06-13 14:26:58 +0500

    Deleted 'staticfiles'

--- juniorcode/staticfiles/main/js/main.js
+++
@@ -1,8 +0,0 @@
-const menuIMG = document.getElementById('menu-img');
-document.addEventListener('mousemove', (event) => {
-    const x = -((event.clientX * .3) / 8);
-    const y = -((event.clientY * .3) / 8);
-
-    menuIMG.style.transform = `translate(${x}px, ${y}px)`;
-    menuIMG.style.transition = 'transform 0.1s ease';
-});
